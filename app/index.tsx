1f3b7a7ec637205431b2d8b88fa26e790c67ef6f
pollob debnath <pullobdebnath@gmail.com>
2026-02-16 00:18:26 +0600

    front page done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,91 @@
-import { Text, View } from "react-native";
+import Colors from "@/services/Colors";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontFamily: "appFontBold" }}>This is my App.</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={{
+          width: "100%",
+          height: 250,
+          marginTop: 80,
+          marginBottom: 30,
+        }}
+      />
+      <View>
+        <Text style={styles.heading}>Welcome to our App</Text>
+        <Text style={styles.heading}>LocalDex</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: Colors.WHITE,
+          margin: 20,
+          padding: 25,
+          borderRadius: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "appFont",
+            fontSize: 18,
+            textAlign: "center",
+          }}
+        >
+          Discover thousands of local Business all in one place
+        </Text>
+        <View style={styles.button}>
+          <Text
+            style={{
+              fontFamily: "appFont",
+              textAlign: "center",
+              fontSize: 18,
+              color:'white'
+            }}
+          >
+            Sign in with Google
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.button,
+            {
+              backgroundColor: Colors.PRIMARY,
+            },
+          ]}
+        >
+          <Text
+            style={{
+              fontFamily: "appFont",
+              textAlign: "center",
+              fontSize: 18,
+              color:'white',
+            }}
+          >
+            Skip
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.PRIMARY,
+    height: "100%",
+  },
+  heading: {
+    fontFamily: "appFontBold",
+    fontSize: 30,
+    color: Colors.WHITE,
+    // marginTop: 40,
+    textAlign: "center",
+  },
+  button: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 50,
+    marginTop: 10,
+    backgroundColor:Colors.BLACK,
+  },
+});
---
+++ services/Colors.tsx
@@ -0,0 +1,6 @@
+export default{
+    PRIMARY:'#37B85E',
+    WHITE:'#fff',
+    BLUE:'#1A77F2',
+    BLACK:'#47698F',
+}
